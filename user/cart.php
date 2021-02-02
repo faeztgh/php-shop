9b34bfccd1b77b6bbcb25f795891425f8f01d2bc
faez <faeztgh@users.noreply.github.com>
2021-02-02 16:38:39 +0330

    some cart update

--- user/cart.php
+++ user/cart.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 require('../config/db.php');
 //Setting up cart
 $productId = $quantity = "";
@@ -87,6 +88,8 @@ if (isset($_POST['checkout'])) {
 }
 
 
+=======
+>>>>>>> parent of 3a2e5c1... make cart
 ?>
 <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="assets/css/cart.css">
@@ -98,6 +101,7 @@ if (isset($_POST['checkout'])) {
                     <div class="col">
                         <h4><b>Shopping Cart</b></h4>
                     </div>
+<<<<<<< HEAD
                     <div class="col align-self-center text-right text-muted">Items: <?php echo $totalItem ?></div>
                 </div>
             </div>
@@ -175,5 +179,66 @@ if (isset($_POST['checkout'])) {
             <a class="btn btn-outline-dark w-100 mt-2" href="../shop.php">Continue Shopping</a>
         </div>
         </form>
+=======
+                    <div class="col align-self-center text-right text-muted">3 items</div>
+                </div>
+            </div>
+            <div class="row border-top border-bottom">
+                <div class="row main align-items-center">
+                    <div class="col-2"><img class="img-fluid" src="https://i.imgur.com/1GrakTl.jpg"></div>
+                    <div class="col">
+                        <div class="row text-muted">Shirt</div>
+                        <div class="row">Cotton T-shirt</div>
+                    </div>
+                    <div class="col"><a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a></div>
+                    <div class="col">&euro; 44.00 <span class="close">&#10005;</span></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="row main align-items-center">
+                    <div class="col-2"><img class="img-fluid" src="https://i.imgur.com/ba3tvGm.jpg"></div>
+                    <div class="col">
+                        <div class="row text-muted">Shirt</div>
+                        <div class="row">Cotton T-shirt</div>
+                    </div>
+                    <div class="col"><a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a></div>
+                    <div class="col">&euro; 44.00 <span class="close">&#10005;</span></div>
+                </div>
+            </div>
+            <div class="row border-top border-bottom">
+                <div class="row main align-items-center">
+                    <div class="col-2"><img class="img-fluid" src="https://i.imgur.com/pHQ3xT3.jpg"></div>
+                    <div class="col">
+                        <div class="row text-muted">Shirt</div>
+                        <div class="row">Cotton T-shirt</div>
+                    </div>
+                    <div class="col"><a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a></div>
+                    <div class="col">&euro; 44.00 <span class="close">&#10005;</span></div>
+                </div>
+            </div>
+            <div class="back-to-shop"><a href="#">&leftarrow;</a><span class="text-muted">Back to shop</span></div>
+        </div>
+        <div class="col-md-4 summary">
+            <div>
+                <h5><b>Summary</b></h5>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col" style="padding-left:0;">ITEMS 3</div>
+                <div class="col text-right">&euro; 132.00</div>
+            </div>
+            <form>
+                <p>SHIPPING</p> <select>
+                    <option class="text-muted">Standard-Delivery- &euro;5.00</option>
+                </select>
+                <p>GIVE CODE</p> <input id="code" placeholder="Enter your code">
+            </form>
+            <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
+                <div class="col">TOTAL PRICE</div>
+                <div class="col text-right">&euro; 137.00</div>
+            </div>
+            <button class="btn">CHECKOUT</button>
+        </div>
+>>>>>>> parent of 3a2e5c1... make cart
     </div>
 </div>
